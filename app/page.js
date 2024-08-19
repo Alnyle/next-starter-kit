67d90edfa8d5f303d518dee2afb468a1249b4738
Alnyle <corgoo7z49h@gmail.com>
2024-08-19 20:08:59 +0300

    initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='mt-4 text-3xl font-bold'>NextJs 14 Starter Kit</h1>
+      </div>
+    </section>
+  )
+}
